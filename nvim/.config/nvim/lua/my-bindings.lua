23f72443fbe3a64c6fef1afd7971ff59b169d117
William Johnson dos Santos Okano <williamokano@gmail.com>
2024-01-26 04:32:47 +0100

    feat: neovim without nvchad for testing

---
+++ nvim/.config/nvim/lua/my-bindings.lua
@@ -0,0 +1,13 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+
+-- line number config
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.g.mapleader = " "
+vim.keymap.set('i', 'jj', '<Esc>')
+
+
---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,13 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = { "lua", "javascript" },
+            highlight = { enabled = true },
+            indent = { enable = true },
+        })
+    end
+}
+
