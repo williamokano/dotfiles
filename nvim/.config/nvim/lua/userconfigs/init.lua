4126247ffb70fa24c81c3f04b99cee829e2664d1
William Johnson dos Santos Okano <williamokano@gmail.com>
2024-02-15 21:52:18 +0100

    feat: adding motion to remove between colons (and others)
    Signed-off-by: William Johnson dos Santos Okano <williamokano@gmail.com>

--- nvim/.config/nvim/lua/userconfigs/init.lua
+++ nvim/.config/nvim/lua/userconfigs/init.lua
@@ -10,3 +10,11 @@ vim.opt.relativenumber = true
 vim.g.mapleader = " "
 vim.keymap.set('i', 'jj', '<Esc>')
 
+vim.keymap.set('n', '<leader>ci', function()
+    local key = vim.fn.nr2char(vim.fn.getchar())
+    local command = "T" .. key .. "dt" .. key .. "i"
+    local feedkeysstr = vim.api.nvim_replace_termcodes(command, true, true, true)
+    vim.api.nvim_feedkeys(feedkeysstr, 'n', true)
+
+    return ""
+end)
