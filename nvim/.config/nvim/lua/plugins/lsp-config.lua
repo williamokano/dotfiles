4f41a76ef2e4f023b76715f1feaa44035f6882f6
William Johnson dos Santos Okano <williamokano@gmail.com>
2024-02-15 21:50:32 +0100

    feat: adding gopls lsp
    Signed-off-by: William Johnson dos Santos Okano <williamokano@gmail.com>

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
+                ensure_installed = { "lua_ls", "gopls" }
             })
         end
     },
@@ -18,6 +18,7 @@ return {
         config = function()
             local lspconfig = require('lspconfig')
             lspconfig.lua_ls.setup({})
+            lspconfig.gopls.setup({})
 
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
