490e759896ef58f1e1700192add5f826c27bfbc4
William Okano <williamokano@gmail.com>
2024-09-01 17:17:43 +0200

    feat: added shortcuts for navigating and closing tabs
    You can switch tabs using tab (or shift tab to backwards) and <leader>bw
    to close current buffer

--- nvim/.config/nvim/lua/williamokano/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/williamokano/plugins/bufferline.lua
@@ -2,11 +2,18 @@ return {
   "akinsho/bufferline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "slant",
-    },
-  },
-}
+  config = function()
+    local opts = {
+      options = {
+        -- mode = "tabs",
+        separator_style = "slant",
+      },
+    }
 
+    require("bufferline").setup(opts)
+
+    vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Cycle to the next tab" })
+    vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Cycle to the prev tab" })
+    vim.keymap.set("n", "<leader>bw", "<Cmd>bdelete<CR>", { desc = "Close current tab" })
+  end,
+}
