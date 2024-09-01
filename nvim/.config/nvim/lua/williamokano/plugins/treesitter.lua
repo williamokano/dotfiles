dfb8360000d5d4673fdfa398eb330ae3ab1bf761
William Okano <williamokano@gmail.com>
2024-09-01 15:16:23 +0200

    feat: changed selection from ctrl space to ctrl shift space
    This change was made due <C-Space> is the leader key for my alacritty

--- nvim/.config/nvim/lua/williamokano/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/williamokano/plugins/treesitter.lua
@@ -52,10 +52,10 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
+          init_selection = "<C-S-Space>",
+          node_incremental = "<C-S-Space>",
           scope_incremental = false,
-          node_decremental = "<bs>",
+          node_decremental = "<BS>",
         },
       },
     })
